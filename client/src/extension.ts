30fa3ea60499a95b920fb9ff7973d2157be8f935
Malintha1996 <malintha1996@gmail.com>
2019-11-03 08:23:20 +0530

    Enable debug mode in client/src/ extension.ts

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -12,12 +12,12 @@ export function activate(context:ExtensionContext){
    const SIDDHI_HOME=path.join(__dirname, '..', 'lib','*');
    classPath=SIDDHI_HOME+':'+classPath
    const args: string[] = ['-cp',classPath];
-   //process.env.LSDEBUG="true";
+   process.env.LSDEBUG="true";
    //'-Xdebug','-Xnoagent','-Djava.compiler=NONE','-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005,quiet=y',
-   // if (process.env.LSDEBUG === "true") {
-   //    //console.log('LSDEBUG is set to "true". Services will run on debug mode');
-   //    args.push('-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005,quiet=y');
-   // }
+   if (process.env.LSDEBUG === "true") {
+      //console.log('LSDEBUG is set to "true". Services will run on debug mode');
+      args.push('-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005,quiet=y');
+   }
    let serverOptions:ServerOptions={
       command:excecutable,
       args: [...args, main],
